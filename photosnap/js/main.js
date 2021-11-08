5d1c39dc85e1216d2d77096f9ac6b4db9f6e3c1d
lilliann482 <54781413+lilliann482@users.noreply.github.com>
2021-11-07 21:32:42 -0500

    Added Photosnap Project
    Includes design system, home page, and project card to portfolio home

---
+++ photosnap/js/main.js
@@ -0,0 +1,21 @@
+// Mobile Navigation
+
+const mainNav = document.getElementById('main-nav')
+const mobNavToggle = document.getElementById('mob-nav-toggle')
+
+mobNavToggle.addEventListener('click', e => {
+    if (e.target.matches('img')) {
+        const img = e.target
+        const status = img.dataset.status
+
+        if (status === 'closed') {
+            img.src = "assets/shared/mobile/close.svg"
+            img.setAttribute('data-status', 'open')
+            mainNav.style.display = "grid"
+        } else if (status === 'open') {
+            img.src = "assets/shared/mobile/menu.svg"
+            img.setAttribute('data-status', 'closed')
+            mainNav.style.display = "none"
+        }
+    }
+})
